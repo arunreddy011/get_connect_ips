7ac9ac6251d970390dbc4e97b6f48da6810f34ad
Arunkumarreddy yalate <arunkumarreddy.yalate@mutualofomaha.com>
2019-08-26 17:08:06 -0500

    initial commit

---
+++ ipranges.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+newpath = '/newfile.txt'
+newfile = open(newpath, 'w')
+ip_ranges = requests.get('https://ip-ranges.amazonaws.com/ip-ranges.json').json()['prefixes']
+connect_ranges = [] #get ready to build a big list
+
+#Amazon Connect needs access to AMAZON_CONNECT, EC2, and CLOUDFRONT over port 443
+connect_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "AMAZON_CONNECT") and (item["region"] == "us-east-1")]
+for ip in connect_ips:
+    connect_ranges.append(ip)
+
+ec2_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "EC2") and (item["region"] == "us-east-1")]
+for ip in ec2_ips:
+    connect_ranges.append(ip)
+
+cloudfront_ips = [item['ip_prefix'] for item in ip_ranges if (item["service"] == "CLOUDFRONT") and (item["region"] == "us-east-1")]
+for ip in cloudfront_ips:
+    connect_ranges.append(ip)
+
+print(connect_ranges)
+newfile.write(connect_ips)
